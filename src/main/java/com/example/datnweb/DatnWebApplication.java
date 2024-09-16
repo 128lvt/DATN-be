9708eb074984d8038e69f49b3c55af9725f055d7
128lvt <128vanthoai@gmail.com>
2024-09-16 17:57:03 +0700

    first commit

---
+++ src/main/java/com/example/datnweb/DatnWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.datnweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatnWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/datnweb/DatnWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datnweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatnWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
