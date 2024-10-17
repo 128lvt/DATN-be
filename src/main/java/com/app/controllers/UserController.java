8b9bbc9a3653792f78f4e556a5c21251d0cac55a
128lvt <128vanthoai@gmail.com>
2024-10-17 15:57:24 +0700

    fix loi

--- src/main/java/com/app/controllers/UserController.java
+++ src/main/java/com/app/controllers/UserController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.dtos.UserDTO;
+
 import jakarta.validation.Valid;
 
 @RestController
