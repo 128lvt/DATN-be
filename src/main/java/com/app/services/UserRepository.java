e290b085a25a7eaa5a1d529212087126702dbfa5
luukhang2k4 <luuvankhang2004@gmail.com>
2024-10-27 19:40:55 +0700

    them UserRepository

---
+++ src/main/java/com/app/services/UserRepository.java
@@ -0,0 +1,13 @@
+package com.app.services;
+
+import com.app.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByPhoneNumber(String phoneNumber);
+
+    Optional<User> findByPhoneNumber(String phoneNumber);
+    //SELECT * FROM users WHERE phoneNumber=?
+}
