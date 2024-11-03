30fa082b7640971ed7bb0fab99be640d897db9a6
CongHau <kaihavertz1005@gmail.com>
2024-11-03 23:26:27 +0700

    them ProductImageDTO

---
+++ src/main/java/com/app/dtos/ProductImageDTO.java
@@ -0,0 +1,16 @@
+package com.app.dtos;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+
+public class ProductImageDTO {
+    @JsonProperty ("product_id")
+    @Min(value = 1, message = "Product's ID muse be > 0 ")
+    private long productId;
+
+    @Size(min = 2, max = 200, message = "Image's name")
+    @JsonProperty ("imageUrl")
+    private String imageUrl;
+}
