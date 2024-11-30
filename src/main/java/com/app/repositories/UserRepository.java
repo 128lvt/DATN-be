a8e99a266ed88cdea241bdbc85900be282407d42
tienloc27 <buitienloc277@gmail.com>
2024-11-30 08:32:41 +0700

    UserRepository fix

--- src/main/java/com/app/repositories/UserRepository.java
+++ src/main/java/com/app/repositories/UserRepository.java
@@ -6,8 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByPhoneNumber(String phoneNumber);
-
-    Optional<User> findByPhoneNumber(String phoneNumber);
-    //SELECT * FROM users WHERE phoneNumber=?
+    Optional<User> findByEmail(String email);
 }
