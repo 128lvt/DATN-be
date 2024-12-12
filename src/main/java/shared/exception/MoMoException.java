61d7a7c03e57c243f0b3c071664710f66a94a371
huydcps <huydcps34798@fpt.edu.vn>
2024-12-12 19:52:13 +0700

    update share

---
+++ src/main/java/shared/exception/MoMoException.java
@@ -0,0 +1,12 @@
+package shared.exception;
+
+public class MoMoException extends Exception {
+    public MoMoException(String message) {
+        super(message);
+    }
+
+    public MoMoException(Throwable cause) {
+        super(cause);
+    }
+
+}
