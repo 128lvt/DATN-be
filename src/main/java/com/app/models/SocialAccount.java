3b3aef48d23c699190f16bfb47136bd395329d12
huydcps <huydcps34798@fpt.edu.vn>
2024-12-15 20:07:19 +0700

    update share

--- src/main/java/com/app/models/SocialAccount.java
+++ src/main/java/com/app/models/SocialAccount.java
@@ -13,19 +13,3 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class SocialAccount {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "provider", nullable = false, length = 20)
-    private String provider;
-
-    @Column(name = "provider_id", length = 20)
-    private String providerId;
-
-    @Column(name = "name", length = 150)
-    private String name;
-
-    @Column(name = "email", length = 150)
-    private String email;
-}
